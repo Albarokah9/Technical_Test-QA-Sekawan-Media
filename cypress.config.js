24c42a2f0c0ea63ebed52b6832868c353f5c7cde
Albarokah9 <albarokahrifansahsutantoputra9@gmail.com>
2025-10-01 12:42:52 +0700

    Push

--- cypress.config.js
+++ cypress.config.js
@@ -1,30 +1,3 @@
-// // cypress.config.js
-// const allureWriter = require('@shelex/cypress-allure-plugin/writer');
-// const { defineConfig } = require('cypress');
-
-// module.exports = defineConfig({
-//     e2e: {
-//         // 1. Set the PRIMARY reporter to the multi-reporter utility
-//         reporter: 'cypress-multi-reporters',
-//         experimentalStudio: true,
-//         // 2. Point the reporter to the config file for reporter options
-//         reporterOptions: {
-//             configFile: 'reporter-config.json',
-//         },
-
-//         setupNodeEvents(on, config) {
-//             // 3. This runs in the Node context and handles result writing
-//             allureWriter(on, config);
-//             return config;
-//         },
-//         baseUrl: 'https://aps-rejanglebong.skwn.dev/dev',
-//         video: true,
-//         screenshotOnRunFailure: true,
-//         screenshotsFolder: 'cypress/screenshots',
-//         videosFolder: 'cypress/videos',
-//     },
-// });
-// cypress.config.js (di root repo)
 const allureWriter = require('@shelex/cypress-allure-plugin/writer');
 const { defineConfig } = require('cypress');
 
@@ -34,28 +7,27 @@ module.exports = defineConfig({
     experimentalStudio: true,
 
     reporter: 'cypress-multi-reporters',
-    reporterOptions: {
-      configFile: 'reporter-config.json',
-    },
+    reporterOptions: { configFile: 'reporter-config.json' },
 
     setupNodeEvents(on, config) {
-      // menulis hasil ke allure-results
       allureWriter(on, config);
       return config;
     },
+
+    // ⇩ bantu stabil di CI
+    defaultCommandTimeout: 10000,
+    pageLoadTimeout: 60000,
+    requestTimeout: 15000,
+    responseTimeout: 30000,
+    retries: { runMode: 2, openMode: 0 },
 
     video: true,
     screenshotOnRunFailure: true,
     screenshotsFolder: 'cypress/screenshots',
     videosFolder: 'cypress/videos',
   },
-
-  // ➜ tambahkan env ini (opsional tapi direkomendasikan)
   env: {
     allure: true,
-    allureResultsPath: 'allure-results'
-    // opsi lain jika perlu:
-    // allureAttachRequests: true,
-    // allureLogCypress: true,
+    allureResultsPath: 'allure-results',
   },
 });
